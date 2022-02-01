da716eeb6f0e2f8f6e3ab5d731685cdc7f3d755e
FlorianTo <florian.tousch@hotmail.fr>
2022-02-01 12:54:50 +0100

    Add `TabBarItemsPreferenceKey`

---
+++ CustomTabBar/TabBarItemsPreferenceKey.swift
@@ -0,0 +1,37 @@
+//
+//  TabBarItemsPreferenceKey.swift
+//  CustomTabBar
+//
+//  Created by Florian on 01/02/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct TabBarItemsPreferenceKey: PreferenceKey {
+    
+    static var defaultValue: [TabBarItem] = []
+    
+    static func reduce(value: inout [TabBarItem], nextValue: () -> [TabBarItem]) {
+        value += nextValue()
+    }
+}
+
+struct TabBarItemsViewModifier: ViewModifier {
+    
+    let tab: TabBarItem
+    @Binding var selection: TabBarItem
+    
+    func body(content: Content) -> some View {
+        content
+            .opacity(selection == tab ? 1.0 : 0.0)
+            .preference(key: TabBarItemsPreferenceKey.self, value: [tab])
+    }
+}
+
+extension View {
+    
+    func tabBarItem(tab: TabBarItem, selection: Binding<TabBarItem>) -> some View {
+        modifier(TabBarItemsViewModifier(tab: tab, selection: selection))
+    }
+}
