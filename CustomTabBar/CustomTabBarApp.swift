d749592dce718b39a9665166fc9a3d60ff320bbb
FlorianTo <florian.tousch@hotmail.fr>
2022-02-01 12:51:33 +0100

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Florian on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
