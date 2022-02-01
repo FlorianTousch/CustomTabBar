d263bc1c4dfef01317ffa21cb245ba409fe2a105
FlorianTo <florian.tousch@hotmail.fr>
2022-02-01 13:02:15 +0100

    Make preview

--- CustomTabBar/ContentView.swift
+++ CustomTabBar/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tabSelection: TabBarItem = .home
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        CustomTabBarContainerView(selection: $tabSelection) {
+            Color.red
+                .tabBarItem(tab: .home, selection: $tabSelection)
+            
+            Color.blue
+                .tabBarItem(tab: .favorites, selection: $tabSelection)
+
+            Color.green
+                .tabBarItem(tab: .profile, selection: $tabSelection)
+
+        }
     }
 }
 
